9c1377d0197a2cc42bd5a36f5c766cab7795eb09
Arnix <arnaud.hote@gmail.com>
2022-12-11 16:32:53 +0100

    Blinking LED GP0

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,54 @@
-//! Blinks the LED on a Pico board
+//! Blinks a LED on a Maker Pi RP2040
 //!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+//! This will blink the LED attached to GPIO0.
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+// The macro for our start-up function
+use cortex_m_rt::entry;
 use defmt::*;
 use defmt_rtt as _;
+
+// GPIO traits
 use embedded_hal::digital::v2::OutputPin;
+
 use panic_probe as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-use rp_pico as bsp;
+// Make an alias for our board support package so copying examples to other boards is easier
+use cytron_maker_pi_rp2040 as bsp;
 
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+// Pull in any important traits
+use bsp::hal::prelude::*;
 
+// A shorter alias for the Peripheral Access Crate, which provides low-level
+// register access
+use bsp::hal::pac;
+
+// A shorter alias for the Hardware Abstraction Layer, which provides
+// higher-level drivers.
+use bsp::hal;
+
+/// Entry point to our bare-metal application.
+///
+/// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
+/// as soon as all global variables are initialised.
+///
+/// The function configures the RP2040 peripherals, then blinks the LED in an
+/// infinite loop.
 #[entry]
 fn main() -> ! {
-    info!("Program start");
+    // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
+    let clocks = hal::clocks::init_clocks_and_plls(
+        bsp::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -42,8 +59,14 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
+    // The delay object lets us wait for specified amounts of time (in
+    // milliseconds)
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins up according to their function on this particular board
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -51,7 +74,7 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mut led_pin = pins.grove_1_a.into_push_pull_output();
 
     loop {
         info!("on!");
